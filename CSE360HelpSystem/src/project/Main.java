6aef3f4cd43fdd0833d7e133cacc14703749b5bf
Xavier <xman123475@gmail.com>
2024-10-03 13:25:28 -0700

    my first commit

--- CSE360HelpSystem/src/project/Main.java
+++ CSE360HelpSystem/src/project/Main.java
@@ -2,7 +2,7 @@ package project;
 public class Main {
     public static void main(String[] args) {
         // This is the entry point of the program
-        System.out.println("Hello, World! again");
+        System.out.println("Hello, World!");
         
         // You can add more code here to implement functionality
     }

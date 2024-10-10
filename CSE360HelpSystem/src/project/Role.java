62cbca7f162dd24cddc737cc3d0517fb195586d3
KapuuZapuu <hassankhanps4@gmail.com>
2024-10-09 22:16:24 -0700

    Update Role.java

--- CSE360HelpSystem/src/project/Role.java
+++ CSE360HelpSystem/src/project/Role.java
@@ -1,9 +1,18 @@
 package project;
 
+/*******
+ * <p> Role class </p>
+ * 
+ * <p> Description:   Lists the roles a user can play, for example, ADMIN, STUDENT, INSTRUCTOR.</p> 
+ * <p> Offers role-checking logic for inputting access to features in code according to the user's role.</p>
+ * 
+ * @author Hassan Khan, Colby Taylor, Xavier Flores, Shashwat Balaji, Avinash Poguluri, Abil Damirbek uulu
+*/
+
 // enum to represent the different roles a user can have
 public enum Role {
     ADMIN,
     INSTRUCTOR,
     STUDENT;
 }
-								
+			

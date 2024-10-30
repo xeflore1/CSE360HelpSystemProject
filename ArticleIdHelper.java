c4dcd1d176a3fa2b22495d9f9235b3a3aa1e06a3
avinashpoguluri <109999341+avinashpoguluri@users.noreply.github.com>
2024-10-30 15:41:49 -0700

    commented ArticleIdHelper.java

--- ArticleIdHelper.java
+++ ArticleIdHelper.java
@@ -4,8 +4,30 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
+/*******
+ * <p> ArticleIdHelper class </p>
+ * 
+ * <p> Description: A class created for generating a unique identifier for help articles based on 
+ * level, groupId, title, authors, abstract, keywords, body, and references. Using the SHA-256 algorithm, 
+ * the class converts the hash to a long integer to be the article's unique identifier. </p>
+ * 
+ * @author Hassan Khan, Colby Taylor, Xavier Flores, Shashwat Balaji, Avinash Poguluri, Abil Damirbek uulu
+*/
+
 public class ArticleIdHelper {
 
+	/*********
+     	* This is the method used to generate the unique identifier using SHA-256 hashing
+     	* 
+    	* @param level    			(beginner, intermediate, advanced, expert)
+    	* @param groupId			identifiers used for groups of related articles
+     	* @param title				title of article
+     	* @param authors			authors of article
+     	* @param articleAbstract	short description about article
+     	* @param keywords			words used to process searches
+     	* @param body				body of help article
+     	* @param references		reference links or similar materials
+     	*/
 	public static long generateArticleId(char[] level, char[] groupId, char[] title, char[] authors, char[] articleAbstract, char[] keywords, char[] body, char[] references) {
         try {
             // Concatenate the fields to form a unique string
@@ -25,6 +47,11 @@ public class ArticleIdHelper {
         }
     }
 
+     /*********
+     * This is the method used to convert bytes to long
+     * 
+     * @param bytes    		the first 8 bytes of the hash	
+     */
     private static long bytesToLong(byte[] bytes) {
         long result = 0;
         for (int i = 0; i < bytes.length; i++) {

fac80bcd35953520d75cfd137d9d360418d04315
xeflore1 <161243535+xeflore1@users.noreply.github.com>
2024-11-25 23:31:04 -0700

    most recent

--- CSE360HelpSystem/src/project/SpecialAccessGroup.java
+++ CSE360HelpSystem/src/project/SpecialAccessGroup.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 // FIXME TEST IF FUNCITONS WORK
 public class SpecialAccessGroup {
-	private static List<SpecialAccessGroup> allGroups = new ArrayList<>();
     private String groupName;
     private List<Long> articles; // Articles with encrypted bodies, long represents the unique id of an article
     private List<User> admins;      // Admins with admin rights for this group, cant view decrypted articles
@@ -20,13 +19,17 @@ public class SpecialAccessGroup {
         this.instructorsWithAccess = new ArrayList<>();
         this.instructorsWithAdminRights = new ArrayList<>();
         this.studentsWithViewingRights = new ArrayList<>();
-        allGroups.add(this);
+    }
+    // loaded constructor
+    public SpecialAccessGroup(String groupName, List<Long> articles, List<User> admins, List<User> instructorsWithAccess, List<User> instructorsWithAdminRights, List<User> studentsWithViewingRights) {
+        this.groupName = groupName;
+        this.articles = articles;
+        this.admins = admins;
+        this.instructorsWithAccess = instructorsWithAccess;
+        this.instructorsWithAdminRights = instructorsWithAdminRights;
+        this.studentsWithViewingRights = studentsWithViewingRights;
     }
 
-    public static List<SpecialAccessGroup> getAllGroups() {
-        return allGroups;
-    }
-    
     // GROUP METHODS
     public String getGroupName() {
         return groupName;
@@ -40,40 +43,41 @@ public class SpecialAccessGroup {
     public List<Long> getArticles() {
         return articles;
     }
-    // given an articles unique long id, add it to the groups article list
-    public void addToArticles(Long uniqueId) {
-    	articles.add(uniqueId);
-    }
-    
-    // given a unique long id, remove an article from article list
-    public void removeFromArticles(Long uniqueId) {
-    	articles.remove(uniqueId);
-    }
-    
     // given a unique long id, check if aricles exists in the list
     public boolean doesArticleExist(Long uniqueId) {
     	for (Long currId : articles) {
     		// article exists
-    		if (currId == uniqueId) {
+    		if (currId.equals(uniqueId)) {
     			return true;
     		}
     	}
     	return false;
     }
+    // given an articles unique long id, add it to the groups article list
+    public boolean addToArticles(Long uniqueId) {
+    	if (doesArticleExist(uniqueId)) {
+    		return false; // return false if article wasn't added
+    	}
+    	else {
+    		articles.add(uniqueId);
+    		return true; // return true if article was added
+    	}
+    }
+    
+    // given a unique long id, remove an article from article list
+    public boolean removeFromArticles(Long uniqueId) {
+    	if (doesArticleExist(uniqueId)) {
+    		articles.remove(uniqueId);
+    		return true; // return true if article was removed
+    	}
+    	else {
+    		return false; // return false if article wasn't removed
+    	}
+    }
     
     // ADMIN METHODS
     public List<User> getAdmins() {
         return admins;
-    }
-    
-    // add user to admin list
-    public void addToAdmins(User user) {
-    	admins.add(user);
-    }
-
-    // remove user from admin list
-    public void removeFromAdmins(User user) {
-    	admins.remove(user);
     }
     
     // checks if user exist in admin list
@@ -86,23 +90,33 @@ public class SpecialAccessGroup {
     	}
     	return false;
     }
+    // add user to admin list
+    public boolean addToAdmins(User user) {
+    	if (doesAdminExist(user)) {
+    		return false; // admin already exists
+    	}
+    	else {
+    		admins.add(user);
+    		return true;
+    	}
+    }
+
+    // remove user from admin list
+    public boolean removeFromAdmins(User user) {
+    	if (doesAdminExist(user)) {
+    		admins.remove(user);
+    		return true; // admin was removed
+    	}
+    	else {
+    		return false;
+    	}
+    }
     
     // INSTRUCTOR WITH ACCESS METHODS
     // add user to instr. with access list
     public List<User> getInstructorsWithAccess() {
         return instructorsWithAccess;
     }
-    
-    // add to instructor with access list
-    public void addToInstrWithAccess(User user) {
-    	instructorsWithAccess.add(user);
-    }
-
-    // remove user from instr. with access list
-    public void removeFromInstrWithAccess(User user) {
-    	instructorsWithAccess.remove(user);
-    }
-    
     // checks if user exist in instr. with access list
     public boolean doesInstrExistInAccessList(User user) {
     	for (User currUser : instructorsWithAccess) {
@@ -113,22 +127,32 @@ public class SpecialAccessGroup {
     	}
     	return false;
     }
+    // add to instructor with access list
+    public boolean addToInstrWithAccess(User user) {
+    	if (doesInstrExistInAccessList(user)) {
+    		return false;
+    	}
+    	else {
+    		instructorsWithAccess.add(user);
+    		return true;
+    	}
+    }
+
+    // remove user from instr. with access list
+    public boolean removeFromInstrWithAccess(User user) {
+    	if (doesInstrExistInAccessList(user)) {
+    		instructorsWithAccess.remove(user);
+    		return true;
+    	}
+    	else {
+    		return false;
+    	}
+    }
 
     // INSTRUCTOR WITH ADMIN RIGHTS METHODS
     public List<User> getInstructorsWithAdminRights() {
         return instructorsWithAdminRights;
     }
-    
-    // add to instructor with admin rights list
-    public void addToInstrWithAdminRights(User user) {
-    	instructorsWithAdminRights.add(user);
-    }
-
-    // remove user from instructor with admin rights list
-    public void removeFromInstrWithAdminRights(User user) {
-    	instructorsWithAdminRights.remove(user);
-    }
-    
     // checks if user exist in instructor with admin rights list
     public boolean doesInstrExistInAdminRightsList(User user) {
     	for (User currUser : instructorsWithAdminRights) {
@@ -139,22 +163,32 @@ public class SpecialAccessGroup {
     	}
     	return false;
     }
+    // add to instructor with admin rights list
+    public boolean addToInstrWithAdminRights(User user) {
+    	if (doesInstrExistInAdminRightsList(user)) {
+    		return false;
+    	}
+    	else {
+    		instructorsWithAdminRights.add(user);
+    		return true;
+    	}
+    }
+
+    // remove user from instructor with admin rights list
+    public boolean removeFromInstrWithAdminRights(User user) {
+    	if (doesInstrExistInAdminRightsList(user)) {
+    		instructorsWithAdminRights.remove(user);
+    		return true;
+    	}
+    	else {
+    		return false;
+    	}
+    }
 
     // STUDENT LIST METHODS
     public List<User> getStudentsWithAccess() {
         return studentsWithViewingRights;
     }
-    
-    // add to student list
-    public void addToStudentList(User user) {
-    	studentsWithViewingRights.add(user);
-    }
-
-    // remove user from student list
-    public void removeFromStudentList(User user) {
-    	studentsWithViewingRights.remove(user);
-    }
-    
     // checks if user exist in student list
     public boolean doesStudentExistInStudentList(User user) {
     	for (User currUser : studentsWithViewingRights) {
@@ -164,5 +198,26 @@ public class SpecialAccessGroup {
     		}
     	}
     	return false;
+    }
+    // add to student list
+    public boolean addToStudentList(User user) {
+    	if (doesStudentExistInStudentList(user)) {
+    		return false;
+    	}	
+    	else {
+    		studentsWithViewingRights.add(user);
+    		return true;
+    	}
+    }
+
+    // remove user from student list
+    public boolean removeFromStudentList(User user) {
+    	if (doesStudentExistInStudentList(user)) {
+    		studentsWithViewingRights.remove(user);
+    		return true;
+    	}	
+    	else {
+    		return false;
+    	}    
     }
 }
